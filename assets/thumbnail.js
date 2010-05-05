69aa84ed7e596c729a60f29e730ba59171a167b2
Ross Cairns <ross@Electricglen.(none)>
2010-05-05 18:48:07 +0000

    swapped x & x. added padding.

--- assets/thumbnail.js
+++ assets/thumbnail.js
@@ -3,16 +3,17 @@
 		$(".field-upload, .selectable").find("a").map(function(){
 			if (this.href.match(/\.(?:bmp|gif|jpe?g|png)$/i)) return this;
 		}).each(function(index,obj) {
-				var height = 50;
+				var height = 100;
 				var width = 150;
 		
 				var link = $(obj);
 				var cell = link.parent();
 				var fileData = link.attr('href').match(/(.*)(workspace\/)(.*(:bmp|gif|jpe?g|png))/);
-				var thumbnailURL = fileData[1] + 'image/1/0/' + width + '/' + fileData[3]
+				var thumbnailURL = fileData[1] + 'image/1/0/' + height + '/' + fileData[3]
 				
 				cell.css('height', height);
-				cell.css('width', width);
+				cell.css('padding', '0 20px 0 20px');
+				//cell.css('width', width);
 				cell.css('background-image', 'url(' + thumbnailURL + ')');
 				cell.css('background-repeat', ' no-repeat');
 
